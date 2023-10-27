a00d78a27f10da3766dd425614c8934987e21318
mhoppitt <mhoppitt18@outlook.com>
2023-10-27 12:59:12 +1100

    Initial Commit

---
+++ Hoppitt Calendar/Hoppitt_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hoppitt_CalendarApp.swift
+//  Hoppitt Calendar
+//
+//  Created by Matt Hoppitt on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hoppitt_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
