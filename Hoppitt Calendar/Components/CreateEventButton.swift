19afed534717000798e4692dea4a7f256fe8a1de
mhoppitt <mhoppitt18@outlook.com>
2023-10-28 12:12:18 +1100

    basic directory template and DynamoDB configuration

--- Hoppitt Calendar/Components/CreateEventButton.swift
+++ Hoppitt Calendar/Components/CreateEventButton.swift
@@ -1,5 +1,5 @@
 //
-//  TestView.swift
+//  CreateEventButton.swift
 //  Hoppitt Calendar
 //
 //  Created by Matt Hoppitt on 27/10/2023.
@@ -11,9 +11,13 @@ struct CreateEventButton: View {
     var body: some View {
         Button(action: {
             Task {
-                let event: CalendarEvent = CalendarEvent(title: "test event", date: "123")
+                let event: CalendarEvent = CalendarEvent(id: "1", title: "test event", date: "123")
                 let eventsTable = CalendarEventsTable()
-                try await eventsTable.addEvent(event: event)
+                do {
+                    try await eventsTable.addEvent(event: event)
+                } catch let error {
+                    print(error)
+                }
             }
         }) {
             Text("Create Event")
---
+++ Hoppitt Calendar/Resources/Secrets.swift
@@ -0,0 +1,11 @@
+//
+//  Secrets.swift
+//  Hoppitt Calendar
+//
+//  Created by Matt Hoppitt on 27/10/2023.
+//
+
+import Foundation
+
+var ACCESS_KEY = "AKIAWY3RQAGDJBGZUFQ7"
+var SECRET_KEY = "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"
